98fbda3d5fade69adf22399fb47c7af4ba2a32a6
VersorVerbi <doroapodeus@gmail.com>
2019-07-16 22:16:22 -0500

    SQL stored procedures initial load

--- functions.sql
+++ functions.sql
@@ -1,1 +1,36 @@
+CREATE FUNCTION `keywordList`(`postQid` INT) RETURNS varchar(2560) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci
+BEGIN
+  DECLARE finished BIT DEFAULT 0;
+  DECLARE keyword INT DEFAULT 0;
+  DECLARE keyword_cursor CURSOR FOR SELECT keywords.tokenId FROM keywords WHERE keywords.postId = postQid;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
+  OPEN keyword_cursor;
+  check_keywords: LOOP
+    FETCH keyword_cursor INTO keyword;
+    IF finished = 1 THEN LEAVE check_keywords; END IF;
+    SET @tf_idf = tfIdfScore(keyword, postQid);
+    IF @tf_idf > 5 THEN
+      SELECT tokens.token FROM tokens WHERE tokens.id = keyword LIMIT 1 INTO @str;
+      SET @output = CONCAT(@output,";",@str);
+    END IF;
+  END LOOP check_keywords;
+  CLOSE keyword_cursor;
+  RETURN @output;
+END
 
+CREATE FUNCTION `tfIdfScore`(`tokenQid` INT, `postQid` INT) RETURNS float
+RETURN
+    (SELECT keywords.num_in_post
+        FROM keywords
+        WHERE keywords.tokenId = tokenQid 
+            AND keywords.postId = postQid
+        LIMIT 1)
+    *
+    (LOG10(
+        (SELECT COUNT(*) FROM posts)
+        *
+        (SELECT tokens.document_count
+            FROM tokens
+            WHERE tokens.id = tokenQid
+            LIMIT 1)
+    ))
--- procedures.sql
+++ procedures.sql
@@ -1,1 +1,83 @@
+CREATE PROCEDURE `closestMatch`(
+    IN `inTokenString` VARCHAR(512),
+    OUT `matchString` VARCHAR(512),
+    OUT `matchId` INT,
+    OUT `matchProximity` FLOAT)
+BEGIN
+    DECLARE finished BIT DEFAULT 0;
+    DECLARE token_string VARCHAR(512) DEFAULT "";
+    DECLARE token_id INT DEFAULT 0;
+    DECLARE max_jac FLOAT DEFAULT 0;
+    DECLARE max_id INT DEFAULT 0;
+    DECLARE max_string VARCHAR(512) DEFAULT "";
 
+    DECLARE token_cursor CURSOR FOR SELECT tokens.id, tokens.token FROM tokens;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
+
+    OPEN token_cursor;
+
+    fuzzy_match: LOOP
+        FETCH token_cursor INTO token_id, token_string;
+        IF finished = 1 THEN LEAVE fuzzy_match; END IF;
+        CALL jaccardScore(inTokenString, token_string, @jac);
+        IF @jac > max_jac THEN
+                SET max_jac = @jac;
+            SET max_id = token_id;
+            SET max_string = token_string;
+        END IF;
+    END LOOP fuzzy_match;
+    SET matchString = max_string;
+    SET matchId = max_id;
+    SET matchProximity = max_jac;
+END
+
+
+CREATE PROCEDURE `jaccardScore`(
+    IN `token1` VARCHAR(512),
+    IN `token2` VARCHAR(512),
+    OUT `score` FLOAT)
+BEGIN
+    DECLARE split_length INT DEFAULT 0;
+    SET @split1 = GREATEST(1,LEAST(3, FLOOR(CHAR_LENGTH(token1) / 3)));
+    SET @split2 = GREATEST(1,LEAST(3, FLOOR(CHAR_LENGTH(token2) / 3)));
+    SET split_length = LEAST(@split1, @split2);
+
+    CREATE TEMPORARY TABLE IF NOT EXISTS token_1_chunks (idx INT, chunk VARCHAR(3));
+    CREATE TEMPORARY TABLE IF NOT EXISTS token_2_chunks (idx INT, chunk VARCHAR(3));
+
+    SET @i = 1;
+    SET @idx = 0;
+    get_chunks_1: LOOP
+        IF @i > CHAR_LENGTH(token1) THEN LEAVE get_chunks_1; END IF;
+        SET @chunk = SUBSTRING(token1, @i, split_length);
+        INSERT INTO token_1_chunks VALUES (@idx, @chunk);
+        SET @idx = @idx + 1;
+        SET @i = @i + 1;
+    END LOOP get_chunks_1;
+
+    SET @i = 1;
+    SET @idx = 0;
+    get_chunks_2: LOOP
+        IF @i > CHAR_LENGTH(token2) THEN LEAVE get_chunks_2; END IF;
+        SET @chunk = SUBSTRING(token2, @i, split_length);
+        INSERT INTO token_2_chunks VALUES (@idx, @chunk);
+        SET @idx = @idx + 1;
+        SET @i = @i + 1;
+    END LOOP get_chunks_2;
+
+    SELECT @intersect_count:=COUNT(*)
+        FROM token_1_chunks t1
+        INNER JOIN token_2_chunks t2
+            ON t1.chunk = t2.chunk;
+    SELECT @union_count:=COUNT(*)
+        FROM (
+            SELECT t1.chunk
+                FROM token_1_chunks t1
+                UNION (
+                    SELECT t2.chunk
+                        FROM token_2_chunks t2
+                )
+        ) AS s1;
+
+    SET score:=(@intersect_count / @union_count);
+END
