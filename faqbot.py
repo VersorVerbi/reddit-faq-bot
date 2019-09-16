d34330c34ac3784ccbb319e0f65cc0cdb03214db
VersorVerbi <doroapodeus@gmail.com>
2019-09-16 15:42:15 -0500

    Remove extraneous passes

--- faqbot.py
+++ faqbot.py
@@ -364,16 +364,12 @@ def test_results(pid_to_test):
         message = process_post(post, False, True)
     except faqhelper.IgnoredFlair:
         message = "Post %s has a flair that is set to be ignored." % pid_to_test
-        pass
     except faqhelper.IgnoredTitle:
         message = "Post %s has text in the title that is set to be ignored." % pid_to_test
-        pass
     except faqhelper.IncorrectPostType:
         message = "Post %s is a stickied post or a link post, so it is set to be ignored." % pid_to_test
-        pass
     except faqhelper.WrongSubreddit:
         message = "Post %s is not on the r/%s subreddit." % (pid_to_test, config.SUBREDDIT)
-        pass
     return "Test results for post (%s)[%s] (PID: %s)\n\n------\n\n%s" % \
            (post.title, post.permalink, pid_to_test, message)
 # endregion
@@ -833,20 +829,16 @@ def handle_command_message(msg):
                 exec(code_to_exec, globals(), locals())
             except mysql.connector.Error:
                 reply_message = sql_failure()
-                pass
             except (faqhelper.MissingParameter, faqhelper.MismatchedParameter, IndexError):
                 reply_message = invalid_params(cmd)
-                pass
             except (faqhelper.BadParameter, faqhelper.IncorrectState, faqhelper.WrongSubreddit):
                 reply_message = improper_params(cmd[0])
-                pass
             try:
                 code_to_exec = 'global reply_message; reply_message = '\
                                + switch(faqhelper.ADMIN_REPLIES, '-1', cmd[0].upper())
                 exec(code_to_exec, globals(), locals())
             except IndexError:
                 reply_message = invalid_params(cmd)
-                pass
         reply_message += admin_signature()
     msg.reply(reply_message)
     return
@@ -871,19 +863,14 @@ def main_loop():
                         process_post(caller)
                     except faqhelper.IgnoredFlair:
                         print("Ignored by flair")
-                        pass
                     except faqhelper.IgnoredTitle:
                         print("Ignored by title")
-                        pass
                     except faqhelper.IncorrectPostType:
                         print("Ignored as sticky or link")
-                        pass
                     except faqhelper.WrongSubreddit:
                         print("Ignored as on wrong subreddit")
-                        pass
                     except faqhelper.AlreadyProcessed:
                         print("Ignored as already processed")
-                        pass
                 else:
                     process_comment(caller)
                 if len(VALID_ADMINS) > 1:
@@ -905,13 +892,11 @@ def main_loop():
         db.close()
         sleep(2)
         db = get_mysql_connection()
-        pass
     except praw.exceptions.PRAWException:
         err_data = sys.exc_info()
         print(err_data)
         r = None
         r = get_reddit()
-        pass
     except Exception as e:
         db.close()
         err_data = sys.exc_info()
